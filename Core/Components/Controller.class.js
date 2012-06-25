d9afa5ee3fb1769be5485417b57e45da28c68afc
naal <ignacio.althabe@lifia.info.unlp.edu.ar>
2012-06-25 11:05:03 -0300

    Merge con branch dev

--- Core/Components/Controller.class.js
+++ Core/Components/Controller.class.js
@@ -4,24 +4,5 @@
  * @extends Component
  */
 var Controller = new Class({
-	Extends : Component,
-	options : {
-		spinner : {
-			style : {
-				'background-color' : 'rgba(0,0,0,0.3)'
-			},
-			message : 'Cargando...',
-			img : {
-				html : '<center><img src="images/spinner.gif"></center>'
-			}
-		}
-	},
-	startSpinner : function(where, text) {
-		this.options.spinner.message = text;
-		this._spinner = new Spinner(where, this.options.spinner);
-		this._spinner.show();
-	},
-	stopSpinner : function() {
-		this._spinner.hide();
-	}
+	Extends : Component
 });
