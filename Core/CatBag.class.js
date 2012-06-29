18f8f1de11957ba344bba20200f29c2dd08eb22b
naal <ignacio.althabe@lifia.info.unlp.edu.ar>
2012-06-29 14:21:51 -0300

    Prenvent Reload off resources

--- Core/CatBag.class.js
+++ Core/CatBag.class.js
@@ -61,6 +61,7 @@ var CatBag = new Class(
 			addResourcesToLoad : function(resourcesName) {
 				resourcesName.each(function(elem) {
 					if (this.resources[elem] == undefined) {
+						this.resources[elem] = false;//Prevent reload...
 						this.app.addResourceToLoad(this, elem);
 						this.resourcesToLoad++;
 						this.app.loadResource();
--- Core/Components/Model.class.js
+++ Core/Components/Model.class.js
@@ -4,28 +4,5 @@
  * @extends Component
  */
 var Model = new Class({
-  Extends: Component,
-  _ready: false,
-  _loading: false,
-  init: function(){
-	  this.ready();
-  },
-  ready: function(){
-	  this._ready = true;
-	  this.fireEvent('ready',[this]);
-  },
-  loading: function(){
-	  this._loading = true;
-	  this.fireEvent('loading',[this]);
-  },
-  updated: function(){
-	  this._loading = false;
-	  this.fireEvent('updated',[this]);
-  },
-  isReady: function(){
-  	return this._ready;
-  },
-  setData: function(data){
-  	this.data = data;
-  }
+  Extends: Component
 });
